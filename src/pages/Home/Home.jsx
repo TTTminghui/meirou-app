aeecfaef8aa59926a1644e4a7b7bce958f89cb1b
liqiang <1903766192@qq.com>
2019-11-05 20:56:08 +0800

    li

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { SearchBar, Carousel, WhiteSpace, WingBlank } from 'antd-mobile';
+import '../../pages/Home/Home.less';
+import { connect } from 'react-redux';
 
-function Home(props){
-    return <div>
-        首页
-    </div>
+class Home extends React.Component {
+    handleClick = () => {
+        this.manualFocusInst.focus();
+    }
+    render() {
+        return <div className="main-box">
+            <h2>美肉商城</h2>
+            <div className="search" style={{backgroundColor:'white'}}>
+                <SearchBar
+                    placeholder="搜索商品"
+                    ref={ref => this.manualFocusInst = ref}
+                />
+                <WhiteSpace />
+            </div>
+            <div className="main-body">
+                <WingBlank>
+                    <Carousel
+                        autoplay={true}
+                        infinite={true}
+                        autoplayInterval='1000'
+                    >
+                        {this.state.data.map(val => (
+                            <a
+                                key={val}
+                                href=""
+                                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                            >
+                                <img
+                                    src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                    alt=""
+                                    style={{ width: '100%', verticalAlign: 'top' }}
+                                    onLoad={() => {
+                                        window.dispatchEvent(new Event('resize'));
+                                        this.setState({ imgHeight: 'auto' });
+                                    }}
+                                />
+                            </a>
+                        ))}
+                    </Carousel>
+                </WingBlank>
+            </div>
+        </div>
+    }
+    state = {
+        data: ['1', '2', '3'],
+        imgHeight: 176,
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            });
+        }, 100);
+    }
 }
 
 export default Home;
